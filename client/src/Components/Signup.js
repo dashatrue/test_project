9338e9cc484101d41beefffa2af8791c639495c4
Dora <dashatrue34@gmail.com>
2025-03-24 23:24:56 +0500

    Commit #5

---
+++ client/src/Components/Signup.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const SignUpPage=()=>{
+    return (
+        <div className="sign-up">
+            <h1>SignUp Page</h1>
+        </div>
+    )
+}
+
+export default SignUpPage
--- client/src/index.js
+++ client/src/index.js
@@ -1,25 +1,32 @@
-import React, {useEffect, useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react'
 import ReactDOM from 'react-dom/client'
+import NavBar from './Components/Navbar';
+
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+} from 'react-router-dom'
+import HomePage from './Components/Home';
+import SignUpPage from './Components/Signup';
 
 const App=()=>{
 
-    useEffect(
-        ()=>{
-            fetch('/recipe/hello')
-            .then(response=>response.json())
-            .then(data=>{console.log(data)
-                setMessage(data.message)
-            })
-            .catch(err=>console.log(err))
-
-        }, []
-    )
-
-    const [message, setMessage]=useState('')
     return (
-        <div className="app">
-            <h1>{message}</h1>
-        </div>
+        <Router>
+            <div className="container">
+                <NavBar/>
+                <Switch>
+                    <Route path="/signup">
+                        <SignUpPage/>
+                    </Route>
+                    <Route path="/">
+                        <HomePage/>
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
     )
 }
 
